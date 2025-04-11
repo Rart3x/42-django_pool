82020b7605451f307aa3c1a151f312d02d378595
Rart3x <ramjattankeny971@gmail.com>
2025-04-11 16:02:03 +0200

    wip: var.py

---
+++ Django 1/ex00/var.py
@@ -0,0 +1,22 @@
+def my_var():
+    """My var function, print 9 variable types"""
+    int = 42
+    str = "42"
+    str2 = "quarante-deux"
+    float = 42.0
+    bool = True
+    list = [42]
+    dict = {42: 42}
+    tuple = (42,)
+    set = {'set()'}
+
+    for var in [int, str, str2, float, bool, list, dict, tuple, set]:
+        print(f"{var} est de type {type(var)}")
+
+
+def main():
+    """Main function"""
+    my_var()
+
+if __name__ == "__main__":
+    main()
