14658438a1ae7c5e0450d91f489d0b48c502f962
Rart3x <ramjattankeny971@gmail.com>
2025-04-14 12:21:52 +0200

    feat: var_to_dict.py

---
+++ Django 0 - Starting/ex02/var_to_dict.py
@@ -0,0 +1,39 @@
+def main():
+    """Main function"""
+    dictionary = {}
+
+    d = [
+        ('Hendrix' , '1942'),
+        ('Allman' , '1946'),
+        ('King' , '1925'),
+        ('Clapton' , '1945'),
+        ('Johnson' , '1911'),
+        ('Berry' , '1926'),
+        ('Vaughan' , '1954'),
+        ('Cooder' , '1947'),
+        ('Page' , '1944'),
+        ('Richards' , '1943'),
+        ('Hammett' , '1962'),
+        ('Cobain' , '1967'),
+        ('Garcia' , '1942'),
+        ('Beck' , '1944'),
+        ('Santana' , '1947'),
+        ('Ramone' , '1948'),
+        ('White' , '1975'),
+        ('Frusciante', '1970'),
+        ('Thompson' , '1949'),
+        ('Burton' , '1939')
+    ]
+
+    for name, year in d:
+        if year not in dictionary:
+            dictionary[year] = []
+        dictionary[year].append(name)
+
+    for year in dictionary:
+        for name in dictionary[year]:
+            print(f"{year} : {name}")
+
+
+if __name__ == "__main__":
+    main()
